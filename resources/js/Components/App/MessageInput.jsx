9c612cfc7e4a652ff23f3b10c405ba54cdb39328
Jgarette0 <aajiliangarette@gmail.com>
2024-05-22 17:34:19 +0800

    streak

--- resources/js/Components/App/MessageInput.jsx
+++ resources/js/Components/App/MessageInput.jsx
@@ -56,6 +56,7 @@ const MessageInput = ({ conversation = null }) => {
             <div className="order-3 xs:order-3 p-2 flex">
                 <button className="p-1 text-gray-400 hover:text-gray-300">
                     <FaceSmileIcon className="w-6 h-6" />
+                    hello world
                 </button>
             </div>
         </div>
