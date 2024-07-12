9dc396502e84a8e1be9df6da073dccf9ca1ece8d
Jgarette0 <aajiliangarette@gmail.com>
2024-07-12 22:34:39 +0800

    documentation

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -175,7 +175,7 @@ export default function Authenticated({ header, children }) {
                     </div>
                 </div>
             </nav>
-
+            //header
             {header && (
                 <header className="bg-white dark:bg-gray-800 shadow">
                     <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
@@ -183,7 +183,6 @@ export default function Authenticated({ header, children }) {
                     </div>
                 </header>
             )}
-
             {children}
         </div>
     );
