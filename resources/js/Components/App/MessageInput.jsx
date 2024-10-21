84aabd4f643e199c362f01d0fbf2d2ec8d3a390f
Jgarette0 <aajiliangarette@gmail.com>
2024-10-21 18:54:06 +0800

    added audio recording functionality and its preview before sending

--- resources/js/Components/App/MessageInput.jsx
+++ resources/js/Components/App/MessageInput.jsx
@@ -15,6 +15,7 @@ import axios from "axios";
 import CustomAudioPlayer from "./CustomAudioPlayer";
 import AttachmentPreview from "./AttachmentPreview";
 import { isAudio, isImage } from "@/helper";
+import AudioRecorder from "./AudioRecorder";
 
 const MessageInput = ({ conversation = null }) => {
     const [newMessage, setNewMessage] = useState("");
@@ -109,6 +110,9 @@ const MessageInput = ({ conversation = null }) => {
         axios.post(route("message.store"), data);
     };
 
+    const recordedAudioReady = (file, url) => {
+        setChosentFiles((prevFiles) => [...prevFiles, { file, url }]);
+    };
     return (
         <div className="flex flex-wrap items-start border-t border-slate-700 py-3">
             <div className="order-2 flex-1 xs:flex-none xs:order-1 p-2">
@@ -130,6 +134,7 @@ const MessageInput = ({ conversation = null }) => {
                         className="absolute left-0 top-0 right-0 bottom-0 z-20 opacity-0 cursor-pointer"
                     />
                 </button>
+                <AudioRecorder fileReady={recordedAudioReady} />
             </div>
             <div className="order-1 px-3 xs-p-0 min-w-[200px] basis-full xs:basis-0 xs:order-2 flex-1 relative">
                 <div className="flex">
