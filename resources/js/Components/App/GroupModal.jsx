9950a6c4f1ec09d40dc33db58b475efde327313d
Jgarette0 <aajiliangarette@gmail.com>
2024-10-23 19:35:00 +0800

    Club Conversation Header Frontend functional

--- resources/js/Components/App/GroupModal.jsx
+++ resources/js/Components/App/GroupModal.jsx
@@ -10,124 +10,115 @@ import PrimaryButton from "../PrimaryButton";
 import { useEventBus } from "@/EventBus";
 import { useEffect, useState } from "react";
 
-export default function GroupModal(show = false, onClose = () => {}) {
-    const page = usePage();
-    const conversations = page.props.conversations;
-    const { on, emit } = useEventBus();
-    const [group, setGroup] = useState({});
-    const { data, setData, processing, reset, post, put, errors } = useForm({
-        id: "",
-        name: "",
-        description: "",
-        user_ids: [],
+export default function GroupModal({ show = false, onClose = () => {} }) {
+  const page = usePage();
+  const conversations = page.props.conversations;
+  const { on, emit } = useEventBus();
+  const [group, setGroup] = useState({});
+  const { data, setData, processing, reset, post, put, errors } = useForm({
+    id: "",
+    name: "",
+    description: "",
+    user_ids: [],
+  });
+
+  const users = conversations.filter((c) => !c.is_group);
+
+  const createOrUpdateGroup = (e) => {
+    e.preventDefault();
+
+    if (group.id) {
+      put(route("group.update", group.id), {
+        onSuccess: () => {
+          closeModal();
+          emit("toast.show", `Group :${data.name} was updated`);
+        },
+      });
+      return;
+    }
+    post(route("group.store"), {
+      onSuccess: () => {
+        emit("toast.show", `Group :${data.name} was created`);
+        closeModal();
+      },
     });
+  };
 
-    const users = conversations.filter((c) => !c.is_group);
+  const closeModal = () => {
+    reset();
+    onClose();
+  };
 
-    const createOrUpdateGroup = (e) => {
-        e.preventDefault();
+  useEffect(() => {
+    return on("GroupModal.show", (group) => {
+      setData({
+        name: group.name,
+        description: group.description,
+        user_ids: group.users
+          .filter((u) => group.owner_id !== u.id)
+          .map((u) => u.id),
+      });
+      setGroup(group);
+    });
+  }, [on]);
 
-        if (group.id) {
-            put(route("group.update", group.id), {
-                onSuccess: () => {
-                    closeModal();
-                    emit("toast.show", `Group :${data.name} was updated`);
-                },
-            });
-            return;
-        }
-        post(route("group.store"), {
-            onSuccess: () => {
-                emit("toast.show", `Group :${data.name} was created`);
-                closeModal();
-            },
-        });
-    };
+  return (
+    <Modal show={show} onClose={closeModal}>
+      <form onSubmit={createOrUpdateGroup} className="p-6 overflow-y-auto">
+        <h2 className="text-xl font-medium text-gray-900 dark:text-gray-100">
+          {group.id ? `Edit Group "${group.name}"` : "Create new Group"}
+        </h2>
 
-    const closeModal = () => {
-        reset();
-        onClose();
-    };
+        <div className="mt-8">
+          <InputLabel htmlFor="name" value="Name" />
 
-    useEffect(() => {
-        return on("GroupModal.show", (group) => {
-            setData({
-                name: group.name,
-                description: group.description,
-                user_ids: group.users
-                    .filter((u) => group.ownder.id !== u.id)
-                    .map((u) => u.uid),
-            });
-            setGroup(group);
-        });
-    }, [on]);
-
-    return (
-        <Modal show={show} onClose={closeModal}>
-            <form
-                onSubmit={createOrUpdateGroup}
-                className="p-6 overflow-y-auto"
-            >
-                <h2 className="text-xl font-medium text-gray-900 dark:text-gray-100">
-                    {group.id
-                        ? `Edit Group "${group.name}"`
-                        : "Create new Group"}
-                </h2>
-
-                <div className="mt-8">
-                    <InputLabel htmlFor="name" value="Name" />
-
-                    <TextInput
-                        id="name"
-                        className="mt-1 block w-full"
-                        value={data.name}
-                        disabled={!!group.id}
-                        onChange={(e) => setData("name", e.target.value)}
-                        required
-                        isFocused
-                    />
-                    <InputError className="mt-2" message={errors.name} />
-                </div>
-                <div className="mt-4">
-                    <InputLabel htmlFor="description" value="Description" />
-                    <TextAreaInput
-                        id="description"
-                        rows="3"
-                        className="mt-1 block w-full"
-                        value={data.description || ""}
-                        onChnage={(e) => setData("description", e.target.value)}
-                    />
-                    <InputError className="mt-2" message={errors.description} />
-                </div>
-                <div className="mt-4">
-                    <InputLabel htmlFor="description" value="Description" />
-                    <UserPicker
-                        value={
-                            users.filter(
-                                (u) =>
-                                    group.owner_id !== u.id &&
-                                    data.user_ids.includes(u.id)
-                            ) || []
-                        }
-                        options={users}
-                        onSelect={(users) =>
-                            setData(
-                                "user_ids",
-                                users.map((u) => u.id)
-                            )
-                        }
-                    />
-                    <InputError className="mt-2" message={errors.user_ids} />
-                </div>
-                <div className="mt-6 flex justify-end">
-                    <SecondaryButton onClick={closeModal}>
-                        cancel
-                    </SecondaryButton>
-                    <PrimaryButton className="ms-3" disabled={processing}>
-                        {group.id ? "Update" : "Create"}
-                    </PrimaryButton>
-                </div>
-            </form>
-        </Modal>
-    );
+          <TextInput
+            id="name"
+            className="mt-1 block w-full"
+            value={data.name}
+            disabled={!!group.id}
+            onChange={(e) => setData("name", e.target.value)}
+            required
+            isFocused
+          />
+          <InputError className="mt-2" message={errors.name} />
+        </div>
+        <div className="mt-4">
+          <InputLabel htmlFor="description" value="Description" />
+          <TextAreaInput
+            id="description"
+            rows="3"
+            className="mt-1 block w-full"
+            value={data.description || ""}
+            onChange={(e) => setData("description", e.target.value)}
+          />
+          <InputError className="mt-2" message={errors.description} />
+        </div>
+        <div className="mt-4">
+          <InputLabel htmlFor="description" value="Select Users" />
+          <UserPicker
+            value={
+              users.filter(
+                (u) => group.owner_id !== u.id && data.user_ids.includes(u.id)
+              ) || []
+            }
+            options={users}
+            onSelect={(users) =>
+              setData(
+                "user_ids",
+                users.map((u) => u.id)
+              )
+            }
+          />
+          <InputError className="mt-2" message={errors.user_ids} />
+        </div>
+        <div className="mt-6 flex justify-end">
+          <SecondaryButton onClick={closeModal}>cancel</SecondaryButton>
+          <PrimaryButton className="ms-3" disabled={processing}>
+            {group.id ? "Update" : "Create"}
+          </PrimaryButton>
+        </div>
+      </form>
+    </Modal>
+  );
 }
--- resources/js/Components/App/GroupUsersPopover.jsx
+++ resources/js/Components/App/GroupUsersPopover.jsx
@@ -1,4 +1,48 @@
-const GroupUsersPopover = () => {
-    return <div>GroupUsersPopover</div>;
-};
-export default GroupUsersPopover;
+import { Popover, Transition } from "@headlessui/react";
+import { UsersIcon } from "@heroicons/react/24/solid";
+import { Link } from "@inertiajs/react";
+import { Fragment } from "react";
+import UserAvatar from "./UserAvatar";
+import { route } from "ziggy-js";
+
+export default function GroupUsersPopover({ users = [] }) {
+  return (
+    <Popover className="relative">
+      {({ open }) => (
+        <>
+          <Popover.Button
+            className={`${
+              open ? "text-gray-200" : "text-gray-400"
+            } hover:text-gray-200 `}
+          >
+            <UsersIcon className="w-4" />
+          </Popover.Button>
+          <Transition
+            as={Fragment}
+            enter="transition ease-in duration-200"
+            enterFrom="opacity-0 translate-y-1"
+            enterTo="opacity-100 translate-y-0"
+            leave="transition ease-in duration-200"
+            leaveFrom="opacity-100 translate-y-0"
+            leaveTo="opacity-0 translate-y-1"
+          >
+            <Popover.Panel className="absolute right-0 z-20 mt-3 w-[200px] max-w-sm  px-4 sm:px-0 ">
+              <div className="bg-gray-800 py-2">
+                {users.map((user) => (
+                  <Link
+                    href={route("chat.user", user.id)}
+                    key={user.id}
+                    className="flex items-center py-2 gap-2 px-3 hover:bg-black/30 "
+                  >
+                    <UserAvatar user={user} />
+                    <div className="text-xs">{user.name}</div>
+                  </Link>
+                ))}
+              </div>
+            </Popover.Panel>
+          </Transition>
+        </>
+      )}
+    </Popover>
+  );
+}
