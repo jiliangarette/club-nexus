934621ddf6b39c85a37dd31a3addbcdf04b93c21
Jgarette0 <aajiliangarette@gmail.com>
2024-10-23 14:59:19 +0800

    remove the unrelated comments

--- postcss.config.js
+++ postcss.config.js
@@ -1,3 +1,5 @@
+// Configures PostCSS to use Tailwind CSS for utility-first styling and Autoprefixer for adding vendor prefixes to CSS for cross-browser compatibility.
+
 export default {
     plugins: {
         tailwindcss: {},
