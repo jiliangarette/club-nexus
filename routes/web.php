edc73114dec26a88886eb0943f01c0965ba218d5
Jgarette0 <aajiliangarette@gmail.com>
2024-10-29 20:01:43 +0800

    community backend controller and route stups

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ Route::middleware(['auth', 'verified', 'active'])->group(function () {
     Route::get('/chat',[ChatController::class, 'chat'])->name('chat');
     Route::get('chat/member/{user}', [MessageController::class, 'byUser'])->name('chat.user');
     Route::get('chat/club/{group}', [MessageController::class, 'byGroup'])->name('chat.group');
+    Route::get('feed/club/{group}', [MessageController::class, 'byGroup'])->name('feed.group');
     Route::post('/message', [MessageController::class, 'store'])->name('message.store');
     Route::delete('/message/{message}', [MessageController::class, 'destroy'])->name('message.destroy');
     Route::get('/message/older/{message}', [MessageController::class, 'loadOlder'])->name('message.loadOlder');
