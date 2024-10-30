fcce67310242c8bde92ea16a562aa99bd978d524
Jgarette0 <aajiliangarette@gmail.com>
2024-10-30 16:57:29 +0800

    enhance popover modal ui

--- resources/js/Components/App/GroupDescriptionPopover.jsx
+++ resources/js/Components/App/GroupDescriptionPopover.jsx
@@ -9,8 +9,8 @@ export default function GroupDescriptionPopover({ description }) {
         <>
           <Popover.Button
             className={`${
-              open ? "text-gray-200" : "text-gray-400"
-            } hover:text-gray-200 `}
+              open ? "text-blue-500" : "text-gray-500"
+            } hover:text-gray-700 `}
           >
             <ExclamationCircleIcon className="w-4" />
           </Popover.Button>
@@ -24,12 +24,12 @@ export default function GroupDescriptionPopover({ description }) {
             leaveTo="opacity-0 translate-y-1"
           >
             <Popover.Panel className="absolute right-0 z-10 mt-3 w-[300px] max-w-sm  px-4 sm:px-0 ">
-              <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-black/5">
-                <div className="bg-gray-800 p-4">
+              <div className="overflow-hidden rounded-lg shadow-lg ring-1 ring-button">
+                <div className="bg-shade p-4">
                   <h2 className="text-lg mb-3">Description</h2>
                   {description && <div className="text-xs">{description}</div>}
                   {!description && (
-                    <div className="text-xs text-center py-4 text-gray-500">
+                    <div className="text-xs text-center py-4 text-gray-700">
                       No description is define.
                     </div>
                   )}
