25c2d0ab3810cc2d97e54a871cadc81962bd4ba4
Jgarette0 <aajiliangarette@gmail.com>
2024-11-06 23:06:01 +0800

    added and updated store related request

--- app/Http/Requests/StorePostRequest.php
+++ app/Http/Requests/StorePostRequest.php
@@ -4,7 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 
-class StoreMessageRequest extends FormRequest
+class StorePostRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -22,9 +22,8 @@ class StoreMessageRequest extends FormRequest
     public function rules(): array
     {
         return [
-        'message' => 'nullable | string',
+        'content' => 'nullable | string',
         'group_id' => 'required_without:receiver_id|nullable|exists:groups,id',
-        'receiver_id' => 'required_without:group_id|nullable|exists:users,id',
         'attachments' => 'nullable|array|max:10',
         'attachments.*' => 'file|max:1024000'
         ];
