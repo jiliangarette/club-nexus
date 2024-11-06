0477cf58be552047dad5ecf97919150c56f4d2a3
Jgarette0 <aajiliangarette@gmail.com>
2024-11-06 23:33:31 +0800

    enhance group avatar ui

--- resources/js/Components/App/GroupAvatar.jsx
+++ resources/js/Components/App/GroupAvatar.jsx
@@ -1,16 +1,15 @@
 import { UsersIcon } from "@heroicons/react/24/solid";
 
-const GroupAvatar = ({}) => {
+const GroupAvatar = ({ data }) => {
   return (
-    <>
-      <div className={`placeholder avatar`}>
-        <div className={`bg-blue-200 text-gray-800 rounded-full w-4 sm:w-8`}>
-          <span className="text-xl">
-            <UsersIcon className="w-3 sm:w-4" />
-          </span>
-        </div>
+    <div className="placeholder avatar tooltip tooltip-right " data-tip={data}>
+      <div className="bg-blue-200 text-gray-800 rounded-full w-4 sm:w-8">
+        <span className="text-xl">
+          <UsersIcon className="w-3 sm:w-[14px]" />
+        </span>
       </div>
-    </>
+    </div>
   );
 };
+
 export default GroupAvatar;
