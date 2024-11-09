ae75f753379e526a3ba50d14342a5b900009401b
Jgarette0 <aajiliangarette@gmail.com>
2024-11-09 17:37:19 +0800

    added a console info details to be displayed in console

---
+++ resources/js/console-info.js
@@ -0,0 +1,12 @@
+import { useEffect } from "react";
+
+const ConsoleInfo = () => {
+  console.info(
+    `%c Welcome to CLUB NEXUS 🎉`,
+    "color: #00b5ff; font-size: 24px; font-weight: bold;"
+  );
+
+  return null;
+};
+
+export default ConsoleInfo;
